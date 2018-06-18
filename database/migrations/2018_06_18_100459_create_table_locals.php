f0a86fbe737eb04540a4353ec10d0b9bd8ef4fdf
=DinhoRyoh <=dyner@hotmail.fr>
2018-06-18 13:23:39 +0000

    ajout des migrations

---
+++ database/migrations/2018_06_18_100459_create_table_locals.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableLocals extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('label');
+            $table->string('description');
+            $table->string('address');
+            $table->integer('floor');
+            $table->integer('door');
+            $table->float('price', 8, 2);
+            $table->integer('type_id');
+            $table->timestamps();
+            $table->string('image_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locals');
+    }
+}
