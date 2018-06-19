da88a424ebec9fa5b052860239a300926027cd7d
Zagym <dylan148@outlook.com>
2018-06-19 09:23:20 +0200

    Ajout logo + admin login

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="img/logo-localup.png" width="300px">
+            <img src="/img/logo-localup.png" width="300px">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
