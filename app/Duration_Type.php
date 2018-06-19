4b0cb9b786a7107229cd7c5ae87b4ff68fcc14f3
=DinhoRyoh <=dyner@hotmail.fr>
2018-06-19 09:37:16 +0000

    ajout du chemin de logout

---
+++ app/Duration_Type.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Duration_Type extends Model
+{
+    protected $table = 'duration_types';
+}
---
+++ app/Local_Type.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Local_Type extends Model
+{
+    protected $table = 'local_types';
+}
