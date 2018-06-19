77f1d85f848199112a569d966b45f0f98812af9c
admin <admin@iMac3-8.local>
2018-06-19 11:43:51 +0200

    ajout de routes

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,47 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('login', 'AdminController@index');
-    Route::post('login', 'AdminController@login');
+//////Côté admin//////
+Route::group(['prefix' => 'admin', 'middleware' => 'isAdmin'], function() {
+
+    //Users
+    Route::get('users', 'UserController@getAllUsers')->name('admin_users');
+    Route::get('user/{id}', 'UserController@getOneUser')->name('admin_user');
+
+    //Levels
+    Route::get('levels', 'LevelController@getAllLevels')->name('admin_levels');
+
+    //Modules
+    Route::get('modules', 'ModuleController@getAllModules')->name('admin_modules');
+    Route::get('Module/{id}', 'ModuleController@getOneModule')->name('admin_module');
+
+    //Locals
+    Route::get('locals', 'LocalController@getAllLocals')->name('admin_locals');
+    Route::get('local/{id}', 'LocalController@getOneLocal')->name('admin_local');
+
+    //Historique
+
+    //Adminer
+    Route::any('adminer', '\Miroc\LaravelAdminer\AdminerAutologinController@index');
 });
-// Adminer
-Route::any('adminer', '\Miroc\LaravelAdminer\AdminerAutologinController@index');
+
+////// Côté Utilisateur //////
+//Users
+Route::get('user', 'UserController@getUser')->name('user');
+
+//Modules
+Route::get('modules', 'ModuleController@getAllModules')->name('modules');
+Route::get('Module/{id}', 'ModuleController@getOneModule')->name('module');
+
+//Locals
+Route::get('locals', 'LocalController@getAllLocals')->name('locals');
+Route::get('local/{id}', 'LocalController@getOneLocal')->name('local');
+
+//Contact
+Route::get('', '')->name('');
+
+//Louer
+
+//Historique
+
+//Panier
