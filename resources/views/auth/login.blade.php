12ddb7fbc75e07543e7d5b9c6fd2215927cb11f1
Zagym <dylan148@outlook.com>
2018-06-19 10:05:32 +0200

    Inscription + Connection en français avec les autres champs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                 {{ csrf_field() }}
 
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="control-label">E-Mail Address</label>
+                    <label for="email" class="control-label">Adresse Email</label>
 
                     <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
@@ -20,7 +20,7 @@
                 </div>
 
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="control-label">Password</label>
+                    <label for="password" class="control-label">Mot de passe</label>
 
                     <input id="password" type="password" class="form-control" name="password" required>
 
@@ -33,13 +33,13 @@
 
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary rounded-0">
-                        Login
+                        Se connecter
                     </button>
                 </div>
 
                 <div class="form-group">
                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                        Forgot Your Password?
+                        Mot de passe oublié ?
                     </a>
                 </div>
             </form>
