12bef874f9d757c0bd368c030e3be279406983ba
Zagym <dylan148@outlook.com>
2018-06-19 10:05:32 +0200

    Inscription + Connection en français avec les autres champs

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,20 +7,32 @@
             <form class="text-center my-5" method="POST" action="{{ route('register') }}">
                 {{ csrf_field() }}
 
-                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                    <label for="name" class="control-label">Name</label>
+                <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
+                    <label for="firstname" class="control-label">Prénom <span>*</span></label>
 
-                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                    <input id="firstname" type="text" class="form-control" name="firstname" value="{{ old('name') }}" required autofocus>
 
-                    @if ($errors->has('name'))
+                    @if ($errors->has('firstname'))
                         <span class="help-block">
-                            <strong>{{ $errors->first('name') }}</strong>
+                            <strong>{{ $errors->first('firstname') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
+                    <label for="lastname" class="control-label">Nom <span>*</span></label>
+
+                    <input id="lastname" type="text" class="form-control" name="lastname" value="{{ old('lastname') }}" required autofocus>
+
+                    @if ($errors->has('lastname'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('lastname') }}</strong>
                         </span>
                     @endif
                 </div>
 
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="control-label">E-Mail Address</label>
+                    <label for="email" class="control-label">Adresse Email <span>*</span></label>
 
                     <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
@@ -31,8 +43,44 @@
                     @endif
                 </div>
 
+                <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
+                    <label for="address" class="control-label">Adresse</label>
+
+                    <input id="address" type="text" class="form-control" name="address">
+
+                    @if ($errors->has('address'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('address') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="control-label">Password</label>
+                    <label for="city" class="control-label">Ville</label>
+
+                    <input id="city" type="text" class="form-control" name="city">
+
+                    @if ($errors->has('city'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('city') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+                    <label for="phone" class="control-label">Téléphone</label>
+
+                    <input id="phone" type="text" class="form-control" name="phone">
+
+                    @if ($errors->has('phone'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('phone') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <label for="password" class="control-label">Mot de passe <span>*</span></label>
 
                     <input id="password" type="password" class="form-control" name="password" required>
 
@@ -44,14 +92,14 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="password-confirm" class="control-label">Confirm Password</label>
+                    <label for="password-confirm" class="control-label">Confirmation <span>*</span></label>
 
                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                 </div>
 
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary">
-                        Register
+                        S'inscrire
                     </button>
                 </div>
             </form>
