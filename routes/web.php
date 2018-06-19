6115b6206b6572c80bda1061a595547a8af5dbaa
Zagym <dylan148@outlook.com>
2018-06-19 09:23:20 +0200

    Ajout logo + admin login

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('login', 'AdminController@index');
+    Route::post('login', 'AdminController@login');
+});
