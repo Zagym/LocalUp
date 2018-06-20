c46a9d9c4ba626da50f0b3701124c0fd8ab36e5e
Zagym <dylan148@outlook.com>
2018-06-20 10:31:48 +0200

    Ajout du listing réductions en admin

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Auth::routes();
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
 
 Route::get('test', function() {
-   return view('admin.listing.local_types');
+   return view('admin.listing.reductions');
 });
 
 Route::get('test2', function() {
