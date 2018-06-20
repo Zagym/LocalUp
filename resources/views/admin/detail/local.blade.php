4e4b71c17a51bc627d2e675bf8c18a0ddf50b53e
Zagym <dylan148@outlook.com>
2018-06-20 11:20:04 +0200

    Ajout du detail d'un local

--- resources/views/admin/detail/local.blade.php
+++ resources/views/admin/detail/local.blade.php
@@ -3,69 +3,85 @@
 @section('content')
 
     {{--
-        Ajouter chaque ration de niveau + les liens modification/suppresion
-        Ne pas oublier de supprimer la class user pr test, et ce commentaire.
+        Ajouter le local, mais aussi envoyer la liste des types.
     --}}
 
     @php
 
-        $user = new class () {
+        $local = new class () {
             public $id = 2;
-            public $lastname = 'Test';
-            public $firstname = 'Test';
-            public $email = 'Test';
+            public $label = 'Test';
+            public $description = 'Test';
             public $address = 'Test';
             public $city = 'Test';
-            public $zip = 'Test';
-            public $phone = 'Test';
-            public $admin = true;
+            public $floor = 'Test';
+            public $door = 'Test';
+            public $capacity = 'Test';
+            public $price = 'test';
+            public $type = 'test';
+            public $image_url = 'test';
         };
+
+        $type = new class () {
+            public $label = 'test';
+        };
+
+        $types[] = $type;
+        $types[] = $type;
 
     @endphp
 
-    <h1>Modification de l'utilisateur {{ $user->id }}</h1>
+    <h1>Modification du local {{ $local->id }}</h1>
 
     <form>
         <div class="form-row">
             <div class="form-group col-md-6">
-                <label for="lastname">Nom</label>
-                <input type="text" class="form-control" name="lastname" value="{{ $user->lastname }}">
+                <label for="label">Libellé</label>
+                <input type="text" class="form-control" name="label" value="{{ $local->label }}">
             </div>
-            <div class="form-group col-md-6">
-                <label for="firstname">Prénom</label>
-                <input type="text" class="form-control" name="firstname" value="{{ $user->firstname }}">
+            <div class="form-group col-md-3">
+                <label for="capacity">Capacité</label>
+                <input type="text" class="form-control" name="capacity" value="{{ $local->capacity }}">
             </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" value="{{ $user->email }}">
-            </div>
-            <div class="form-group col-md-6">
-                <label for="phone">Téléphone</label>
-                <input type="text" class="form-control" name="phone" value="{{ $user->phone }}">
+            <div class="form-group col-md-3">
+                <label for="capacity">Prix</label>
+                <input type="text" class="form-control" name="price" value="{{ $local->price }}">
             </div>
         </div>
         <div class="form-group">
+            <label for="description">Description</label>
+            <textarea class="form-control" name="description" rows="3">{{ $local->description }}</textarea>
+        </div>
+        <div class="form-group">
             <label for="address">Adresse</label>
-            <input type="text" class="form-control" name="address" value="{{ $user->address }}">
+            <input type="text" class="form-control" name="address" value="{{ $local->address }}">
         </div>
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="city">Ville</label>
-                <input type="text" class="form-control" name="city" value="{{ $user->city }}">
+                <input type="text" class="form-control" name="city" value="{{ $local->city }}">
             </div>
-            <div class="form-group col-md-6">
-                <label for="zip">Code postal</label>
-                <input type="text" class="form-control" name="zip" value="{{ $user->zip }}">
+            <div class="form-group col-md-3">
+                <label for="floor">Etage</label>
+                <input type="text" class="form-control" name="floor" value="{{ $local->floor }}">
+            </div>
+            <div class="form-group col-md-3">
+                <label for="door">Porte</label>
+                <input type="text" class="form-control" name="door" value="{{ $local->door }}">
             </div>
         </div>
         <div class="form-row">
-            <div class="form-check form-check-inline col-md-2">
-                <label class="form-check-label" for="admin">
-                    Administrateur
-                </label>
-                <input class="form-check-input" type="checkbox" name="admin" {{ $user->admin ? 'checked' : '' }}>
+            <div class="form-group col-md-6">
+                <label for="type">type</label>
+                <select class="form-control" name="type">
+                    @foreach($types as $type)
+                        <option>{{ $type->label }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group col-md-6">
+                <label for="file">Image du local</label>
+                <input type="file" class="form-control-file" name="file">
             </div>
         </div>
         <button type="submit" class="btn btn-primary">Modifier</button>
