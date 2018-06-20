e6f51928ae708ee709f6a22d66d9595e4bba71f6
Zagym <dylan148@outlook.com>
2018-06-20 09:57:54 +0200

    Modification home + listing des utilisateur en admin.

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -13,7 +13,15 @@
         <div class="col-lg-3 mb-4">
             <div class="locals">
                 <div class="description-offer text-center p-4 bg-light">
-                    <h2 class="mt-0">Nombre de locaux</h2>
+                    <h2 class="mt-0">Nombre de locaux loué</h2>
+                    <p class="lead">5</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-4">
+            <div class="locals">
+                <div class="description-offer text-center p-4 bg-light">
+                    <h2 class="mt-0">Nombre de villes</h2>
                     <p class="lead">18</p>
                 </div>
             </div>
@@ -22,6 +30,14 @@
             <div class="locals">
                 <div class="description-offer text-center p-4 bg-light">
                     <h2 class="mt-0">Nombre d'utilisateurs</h2>
+                    <p class="lead">5 832</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-4">
+            <div class="locals">
+                <div class="description-offer text-center p-4 bg-light">
+                    <h2 class="mt-0">Nombre de modules</h2>
                     <p class="lead">5 832</p>
                 </div>
             </div>
