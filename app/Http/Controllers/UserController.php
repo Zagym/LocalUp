c955ca7c44aa269039c47278b3659a043aaedc83
Zagym <dylan148@outlook.com>
2018-06-20 07:25:34 +0000

    controller user getUsers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function getUsers()
+    {
+        $users = User::all();
+        return view('users', ['users' => $users]);
+    }
     public function getUser(Request $request)
     {
         $user = $request->user();
