d97a19f1150d702fce78b5a6bc4bdba19f2dacaa
Zagym <dylan148@outlook.com>
2018-06-20 10:31:15 +0000

    update dbseed et design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,11 @@
 
 
   <header id="header-home" class="d-flex align-items-end justify-content-end py-5 px-3">
-    <h1 class="text-center text-lg-right my-5 ml-auto mr-auto mr-sm-5 text-uppercase">Montez d'un level en louant<br class="d-none d-xl-block" /> un espace de travail à votre image</h1>
+    <h1 class="text-center text-lg-right my-5 ml-auto mr-auto mr-sm-5 text-uppercase">
+      Montez d'un level en louant<br class="d-none d-xl-block" /> un espace de travail à votre image
+      <br/>
+      <a href="/locations" class="btn btn-primary btn-lg px-3 py-2">Louer un bureau maintenant</a>
+    </h1>
   </header>
 
   <div id="component-homepage" class="pb-5">
