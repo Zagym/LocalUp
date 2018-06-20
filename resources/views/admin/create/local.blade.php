ceb3756eb666abda30a7cf1b2c8e0d3fbe3d7793
dylvn <trochain.dylan@gmail.com>
2018-06-20 20:48:26 +0200

    Creation d'un local en admin. Résolution de bug. Ajout du type hint.

--- resources/views/admin/create/local.blade.php
+++ resources/views/admin/create/local.blade.php
@@ -2,37 +2,37 @@
 
 @section('content')
 
-    
-    <h1>Modification du local "{{ $local->label }}"</h1>
 
-    <form  method="POST" action="{{ route('admin_local_update', ['local' => $local]) }}" enctype="multipart/form-data">
+    <h1>Modification d'un local</h1>
+
+    <form  method="POST" action="{{ route('admin_local_store') }}" enctype="multipart/form-data">
         {{ csrf_field() }}
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="label">Libellé</label>
-                <input type="text" class="form-control" name="label" value="{{ $local->label }}">
+                <input type="text" class="form-control" name="label" value="{{ old('label') }}">
             </div>
             <div class="form-group col-md-3">
                 <label for="capacity">Capacité</label>
-                <input type="text" class="form-control" name="capacity" value="{{ $local->capacity }}">
+                <input type="text" class="form-control" name="capacity" value="{{ old('capacity') }}">
             </div>
             <div class="form-group col-md-3">
                 <label for="capacity">Prix</label>
-                <input type="text" class="form-control" name="price" value="{{ $local->price }}">
+                <input type="text" class="form-control" name="price" value="{{ old('price') }}">
             </div>
         </div>
         <div class="form-group">
             <label for="description">Description</label>
-            <textarea class="form-control" name="description" rows="3">{{ $local->description }}</textarea>
+            <textarea class="form-control" name="description" rows="3">{{ old('description') }}</textarea>
         </div>
         <div class="form-row">
             <div class="form-group col-md-9">
                 <label for="address">Adresse</label>
-                <input type="text" class="form-control" name="address" value="{{ $local->address }}">
+                <input type="text" class="form-control" name="address" value="{{ old('address') }}">
             </div>
             <div class="form-group col-md-3">
                 <label for="zip">Code postal</label>
-                <input type="text" class="form-control" name="zip" value="{{ $local->zip }}">
+                <input type="text" class="form-control" name="zip" value="{{ old('zip') }}">
             </div>
         </div>
         <div class="form-row">
@@ -40,21 +40,17 @@
                 <label for="city">Ville</label>
                 <select class="form-control" name="city_id">
                     @foreach($cities as $city)
-                        @if($city->id == $local->city_id)
-                            <option value="{{$city->id}}" selected="selected">{{ $city->label }}</option>
-                        @else
-                            <option value="{{$city->id}}">{{ $city->label }}</option>
-                        @endif
+                        <option value="{{$city->id}}">{{ $city->label }}</option>
                     @endforeach
                 </select>
             </div>
             <div class="form-group col-md-3">
                 <label for="floor">Etage</label>
-                <input type="text" class="form-control" name="floor" value="{{ $local->floor }}">
+                <input type="text" class="form-control" name="floor" value="{{ old('floor') }}">
             </div>
             <div class="form-group col-md-3">
                 <label for="door">Porte</label>
-                <input type="text" class="form-control" name="door" value="{{ $local->door }}">
+                <input type="text" class="form-control" name="door" value="{{ old('door') }}">
             </div>
         </div>
         <div class="form-row">
@@ -62,11 +58,7 @@
                 <label for="type_id">type</label>
                 <select class="form-control" name="type_id">
                     @foreach($types as $type)
-                        @if($type->id == $local->type_id)
-                            <option value="{{$type->id}}" selected="selected">{{ $type->label }}</option>
-                        @else
-                            <option value="{{$type->id}}">{{ $type->label }}</option>
-                        @endif
+                        <option value="{{$type->id}}">{{ $type->label }}</option>
                     @endforeach
                 </select>
             </div>
@@ -76,11 +68,7 @@
 
             </div>
         </div>
-            @if($local->image_url)
-                <img src="{{asset('storage').'/'.$local->image_url}}" class="img-fluid" alt="">
-            @endif
-        <button type="submit" class="btn btn-primary">Modifier</button>
-        <a href="" class="btn btn-danger">Supprimer</a>
+        <button type="submit" class="btn btn-primary">Créer</button>
     </form>
 
 @endsection
