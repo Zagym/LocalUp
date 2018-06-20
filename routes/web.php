40e6f19e0578f6a95e77952f23927c3287aa34d0
Zagym <dylan148@outlook.com>
2018-06-20 12:31:17 +0200

    Ajout d'un detail pour le ratio de niveau

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Auth::routes();
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
 
 Route::get('test', function() {
-   return view('admin.detail.reduction');
+   return view('admin.detail.level_rate');
 });
 
 Route::get('test2', function() {
